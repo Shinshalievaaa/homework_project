8c81d2b769cc7accd0902b7388d6e771becb5e3f
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-07-23 21:16:51 +0500

    fixed type errors in mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,15 @@
-import string
 from datetime import datetime
-from masks import get_mask_card_number, get_mask_account
+
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(number_card: str) -> str:
     """Обработка информации о картах и о счетах и возвращение замаскированного номерв"""
     words = number_card.split()
     if "счет" in number_card.lower():
-        return number_card.replace(words[-1],'') + get_mask_account(words[-1])
+        return number_card.replace(words[-1],'') + get_mask_account(int(words[-1]))
     else:
-        return number_card.replace(words[-1],'') + get_mask_card_number(words[-1])
+        return number_card.replace(words[-1],'') + get_mask_card_number(int(words[-1]))
 
 
 def get_date(date_string: str) -> str:
