44a132cebf5b56748bc457f4633d2823a42552fa
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-07-23 21:06:18 +0500

    add function get_date

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+import string
+from datetime import datetime
 from masks import get_mask_card_number, get_mask_account
 
 
@@ -10,6 +12,14 @@ def mask_account_card(number_card: str) -> str:
         return number_card.replace(words[-1],'') + get_mask_card_number(words[-1])
 
 
+def get_date(date_string: str) -> str:
+    date_string_to_date = datetime.strptime(date_string[0:10], "%Y-%m-%d").date()
+    formatted_date = date_string_to_date.strftime("%d.%m.%Y")
+    return str(formatted_date)
+
+
 with open(r'tests/test_data.txt', 'r', encoding='utf-8') as file_test:
     for line_file in file_test:
         print(mask_account_card(line_file))
+
+print(get_date("2024-03-11T02:26:18.671407"))
