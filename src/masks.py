40e389bce9aac0a9e3f2510d6f0141a229f0786b
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-07-24 20:26:16 +0500

    fixed errors

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,8 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску"""
-    card_number_str = str(card_number)
-    return card_number_str[0:4] + " " + card_number_str[4:6] + "** **** " + card_number_str[-4:]
+    return card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """Принимает на вход номер счета и возвращает его маску"""
-    account_number_str = str(account_number)
-    return "**" + account_number_str[-4:]
+    return "**" + account_number[-4:]
--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(number_card: str) -> str:
     """Обработка информации о картах и о счетах и возвращение замаскированного номерв"""
-    words = number_card.split()
-    if "счет" in number_card.lower():
-        return number_card.replace(words[-1],'') + get_mask_account(int(words[-1]))
+    name, number = number_card.rsplit(' ', maxsplit=1)
+    if number.lower() == "счет":
+        mask_number = get_mask_account(number)
     else:
-        return number_card.replace(words[-1],'') + get_mask_card_number(int(words[-1]))
+        mask_number = get_mask_card_number(number)
+    return f'{name} {mask_number}'
 
 
 def get_date(date_string: str) -> str:
@@ -17,10 +18,3 @@ def get_date(date_string: str) -> str:
     date_string_to_date = datetime.strptime(date_string[0:10], "%Y-%m-%d").date()
     formatted_date = date_string_to_date.strftime("%d.%m.%Y")
     return str(formatted_date)
-
-
-with open(r'tests/test_data.txt', 'r', encoding='utf-8') as file_test:
-    for line_file in file_test:
-        print(mask_account_card(line_file))
-
-print(get_date("2024-03-11T02:26:18.671407"))
