a9927a7cabbffa1007fefc3e39ec70dc4b42baa7
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-08-03 11:06:36 +0500

    fixed variables

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
 from typing import Iterable, Optional
 
 
-def filter_by_state(list_to_filter: Iterable[dict], state: Optional[str] = "EXECUTED") -> list[dict]:
+def filter_by_state(list_to_filter: Iterable[dict], state_to_filter: Optional[str] = 'EXECUTED') -> list[dict]:
     """Функция принимает список словарей и возвращает список словарей, содержащий только те,
-    у которых ключ state (по умолчанию 'EXECUTED') соответствует переданному значению"""
+    у которых статус state (по умолчанию 'EXECUTED') соответствует переданному значению"""
     filtered_list = []
     for item in list_to_filter:
-        if state == item["state"]:
+        if state_to_filter == item["state"]:
             filtered_list.append(item)
 
     return filtered_list
@@ -15,4 +15,4 @@ def filter_by_state(list_to_filter: Iterable[dict], state: Optional[str] = "EXEC
 def sort_by_date(filtered_list: Iterable[dict], sort_reverse: Optional[bool] = True) -> list[dict]:
     """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание)
     и возвращает новый список, отсортированный по дате"""
-    return sorted(filtered_list, key=lambda item: item["date"], reverse=sort_reverse)
+    return sorted(filtered_list, key=lambda item: item['date'], reverse=sort_reverse)
