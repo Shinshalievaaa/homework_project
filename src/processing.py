b4cdd13dab74e3e9b810713d28509b026174d754
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-08-07 21:02:11 +0500

    fixed errors in linter checks

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import Iterable, Optional
 
 
-def filter_by_state(list_to_filter: Iterable[dict], state_to_filter: Optional[str] = 'EXECUTED') -> list[dict]:
+def filter_by_state(list_to_filter: Iterable[dict], state_to_filter: Optional[str] = "EXECUTED") -> list[dict]:
     """Функция принимает список словарей и возвращает список словарей, содержащий только те,
     у которых статус state (по умолчанию 'EXECUTED') соответствует переданному значению"""
     filtered_list = []
@@ -15,4 +15,6 @@ def filter_by_state(list_to_filter: Iterable[dict], state_to_filter: Optional[st
 def sort_by_date(filtered_list: Iterable[dict], sort_reverse: Optional[bool] = True) -> list[dict]:
     """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание)
     и возвращает новый список, отсортированный по дате"""
-    return sorted(filtered_list, key=lambda item: item['date'], reverse=sort_reverse)
+    if sort_reverse == None:
+        sort_reverse = True
+    return sorted(filtered_list, key=lambda item: item["date"], reverse=sort_reverse)
