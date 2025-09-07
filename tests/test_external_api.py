6c04cf4fd7f43c1fa136a5bcc04de6a6dddd6db1
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-09-07 17:46:20 +0500

    add docstrings

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -11,7 +11,7 @@ API_KEY = os.getenv('API_KEY')
 
 @patch('requests.get')
 def test_get_exchange_rates(mock_get):
-    """"""
+    """тестирование обращения к Exchange Rates Data API"""
     mock_get.return_value.json.return_value = {'result': 1000}
     assert get_exchange_rates("EUR",100) == 1000
     mock_get.assert_called_once_with(f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount=100",
