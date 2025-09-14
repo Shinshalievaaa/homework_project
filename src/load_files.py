96f3ddf7e1155462d67c298bce46440906881057
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-09-14 19:11:08 +0500

    fixed load_files.py

--- src/load_files.py
+++ src/load_files.py
@@ -3,14 +3,25 @@ import pandas as pd
 
 def load_csv_file(path: str) -> list:
     """функция для считывания финансовых операций из CSV"""
-    with open('data/transactions.csv', encoding='UTF') as file:
-        reader = csv.DictReader(file, delimiter=';')
-        return list(reader)
+    data = []
+    try:
+        with open(path, encoding='UTF') as file:
+            try:
+                data = csv.DictReader(file, delimiter=';')
+                return list(data)
+            except csv.Error:
+                return data
+
+    except FileNotFoundError:
+        return data
 
 
 def load_excel_file(path: str) -> list:
     """функция для считывания финансовых операций из Excel"""
-    df = pd.read_excel('data/transactions_excel.xlsx')
-    # Преобразование в список словарей
-    data = df.to_dict('records')
-    return data
+    data = []
+    try:
+        df = pd.read_excel(path)
+        data = df.to_dict('records')
+        return data
+    except FileNotFoundError:
+        return data
