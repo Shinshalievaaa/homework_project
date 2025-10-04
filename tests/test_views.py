4652355655f8bb43a1241485cbd36ec5ef17fb77
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-10-04 15:58:09 +0500

    fixed test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,10 +3,13 @@ import json
 from src.views import main
 
 
-def test_incorrect_date_main():
+@pytest.mark.parametrize("current_date", ["2025-13-26 20:22:07",
+                                          "2024-12-01 28:01:05",
+                                          "2001-00-01 15:59:43"])
+def test_incorrect_date_main(current_date):
     """тестирование некорректной даты для функции main"""
     with pytest.raises(ValueError) as exc_info:
-        main('2025-13-26 20:22:07')
+        main(current_date)
     assert str(exc_info.value) == "Неверный формат даты и времени. Используйте YYYY-MM-DD HH:MM:SS"
 
 
